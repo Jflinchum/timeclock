d03b8063fb09f765629654841c7250c979d224a8
Jflinchum <jonflinchum@gmail.com>
2018-11-17 17:13:00 -0800

    Added TimesButton that was forgotten in previous commit.

---
+++ src/Components/TimesButton/TimesButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TimesButton extends Component {
+
+  render() {
+    if (this.props.show) {
+      return (
+        <div>
+          <button onClick={this.props.onSubmit}>
+            {this.props.name}
+          </button>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+TimesButton.propTypes = {
+  name: PropTypes.string,
+  onSubmit: PropTypes.func,
+  show: PropTypes.bool
+}
+
+export default TimesButton;
